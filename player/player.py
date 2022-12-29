b43e506023bfd258961cbf39a68d5cee6aa00b28
Daniel Carvalho <dl.carvalho@ua.pt>
2022-12-29 22:33:41 +0000

    Refactoring the code

--- player/player.py
+++ player/player.py
@@ -12,26 +12,43 @@ class Player:
     def __init__(self, nick: str, port):
         self.nick = nick
 
+        # Criação da Socket e do Selector
         self.selector = selectors.DefaultSelector()
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.port = port
 
     def connect(self):
+        """
+                Function used to connect the created Socket to the Playing Area. The port passed in the command-line as an argument to this script should be the port where the Playing Area runs.
+                """
+        # Conexão à socket da Playing Area
         self.socket.connect(self.ADDRESS, self.port)
+        self.selector.register(self.socket, selectors.EVENT_READ, self.read_data)
 
-        self.selector.register(self.socket, selectors.EVENT_READ, data=None)
-
+        # Envio da Register Message à Playing Area
         message = {'class': 'Register', 'type': 'Player', 'nick': self.nick}
         send_msg(self.socket, json.dumps(message).encode('UTF-8'))
 
-        data = recv_msg(self.socket)
-        if data == None:
+        # Verificação da resposta recebida
+        msg = recv_msg(self.socket)
+        if msg == None:
             print("None")
+        if msg['class'] == "Register NACK":
+            # Playing Area rejeitou Player
+            print("Register Rejected")
+            print("Shutting down...")
+            exit()
 
-        print(data)
+    def read_data(self, socket):
+        """
+        This function will determine the class of the received Message, and call the code that should be executed when an instance of this Message is received
+        :param socket: The calling socket
+        :return:
+        """
 
     def loop(self):
         while True:
             events = self.selector.select(timeout=None)
             for key, mask in events:
-                print("Livin la vida loca")
+                callback = key.data
+                callback(key.fileobj)
--- playing_area/parea.py
+++ playing_area/parea.py
@@ -6,7 +6,7 @@ import selectors
 import json
 from messages import send_msg, exact_recv, recv_msg
 
-CONNECTED_CLIENTS = {}
+CONNECTED_CLIENTS = {}                              # Dictionary holding the Connected Clients {ID: socket}
 CURRENT_ID = 1
 
 def dispatch( srv_socket ):
@@ -22,7 +22,6 @@ def dispatch( srv_socket ):
         for key, mask in events:
 
             # Check for a new client connection
-
             if key.fileobj == srv_socket:
                 clt_socket, clt_addr = srv_socket.accept()
                 clt_socket.setblocking( True )
@@ -33,19 +32,8 @@ def dispatch( srv_socket ):
                 print( 'Client added' )
 
             # Client data is available for reading
-
             else:
                 msg = recv_msg( key.fileobj )
-
-                '''
-                if isinstance(msg, ProtoBadFormat): # Socket closed
-                    selector.unregister( key.fileobj )
-                    key.fileobj.close()
-                    print( 'Client removed' )
-                    continue
-                elif isinstance(msg, RegisterMessage):
-                    pass 
-                '''
 
                 if msg == None:
                     selector.unregister(key.fileobj)
@@ -53,25 +41,65 @@ def dispatch( srv_socket ):
                     print('Client removed')
                     continue
 
-                reply = None
-                print(msg)
+                read_data(msg, key.fileobj)
 
-                if msg['class'] == "Register":
-                    if msg['type'] == "Caller":
-                        if 0 in CONNECTED_CLIENTS.keys():
-                            reply = {'class': "Register NACK"}
-                        else:
-                            CONNECTED_CLIENTS[0] = key.fileobj
-                            reply = {'class': "Register ACK"}
-                    else:
-                        CONNECTED_CLIENTS[CURRENT_ID] = key.fileobj
-                        CURRENT_ID += 1
-                        reply = {'class': "Register ACK"}
+def read_data(msg, socket):
+    """
+    This function will determine the class of the received Message, and call the code that should be executed when an instance of this Message is received.
+    :param msg: The message received
+    :param socket: The socket that sent the message
+    :return:
+    """
 
-                if reply != None:
-                    reply = json.dumps( reply )
-                    reply = reply.encode( 'UTF-8' )
-                    send_msg( key.fileobj, reply )
+    '''
+    CÓDIGO A SER USADO APÓIS IMPLEMENTAÇÃO DO PROTOCOLO DE MENSAGENS
+        if isinstance(msg, ProtoBadFormat): # Socket closed
+            selector.unregister( key.fileobj )
+            key.fileobj.close()
+            print( 'Client removed' )
+            continue
+        elif isinstance(msg, RegisterMessage):
+            pass 
+    '''
+
+    ''' CÓDIGO USADO PARA TESTE ENQUANTO PROTOCOLO DE MENSAGENS NÃO FOR IMPLEMENTADO '''
+    reply = None
+    print(msg)
+
+    if msg['class'] == "Register":
+        # REGISTER MESSAGE
+        reply = register_new_client(msg, socket)
+
+
+    if reply != None:
+        reply = json.dumps(reply)
+        reply = reply.encode('UTF-8')
+        send_msg(socket, reply)
+
+
+def register_new_client(msg, socket):
+    """
+    Function that will verify a Register Message to check whether the new Client can be registered as a Player/Caller or not.
+    :param msg:
+    :param socket:
+    :return:
+    """
+    reply = None
+
+    if msg['type'] == "Caller":
+        if 0 in CONNECTED_CLIENTS.keys():
+            # We already have a Caller registered in the Playing Area
+            reply = {'class': "Register NACK"}
+        else:
+            CONNECTED_CLIENTS[0] = socket
+            reply = {'class': "Register ACK"}
+    else:
+        CONNECTED_CLIENTS[CURRENT_ID] = socket
+        CURRENT_ID += 1
+        reply = {'class': "Register ACK"}
+
+    return reply
+
 
 def main():
     if len(sys.argv) != 2:
