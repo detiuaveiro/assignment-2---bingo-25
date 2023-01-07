ed45591b800544df52775780de5e595fef048f31
André Butuc <andrebutuc@ua.pt>
2023-01-07 19:49:17 +0000

    clean vsc module added cc readme

--- security/vsc_security.py
+++ security/vsc_security.py
@@ -4,34 +4,15 @@ from cryptography.hazmat.backends import default_backend as db
 from cryptography.hazmat.primitives.asymmetric.padding import PKCS1v15
 from cryptography.hazmat.primitives.hashes import SHA1, Hash
 
-""" auxiliary functions 
-def print_slots(slots):
-    for slot in slots:
-        print(f"\nSlot: {slot}")
 
-        slot_info = pkcs11.getSlotInfo(slot)
-        print(f"Info: \n{slot_info} \n")
-"""
-def list_content_of_card(slot):
-    all_attr = list(PyKCS11.CKA.keys())
-    all_attributes = [e for e in all_attr if isinstance(e, int)]
-    session = pkcs11.openSession(slot)
-    session.login('1111')
-    for obj in session.findObjects():
-        attr = session.getAttributeValue(obj, all_attributes)
-
-        attrDict = dict(list(zip(all_attributes, attr)))
-        print("Type:", PyKCS11.CKO[attrDict[PyKCS11.CKA_CLASS]], "\tLabel1:", attrDict[PyKCS11.CKA_LABEL])
-    session.logout()
-    session.closeSession()
-
-
-
-def get_cert_data(slot):
+def get_cert_data():
     """ get certificate data from card
-    :param slot: slot number
     :return: certificate data
     """
+    lib = '/usr/lib/x86_64-linux-gnu/pkcs11/opensc-pkcs11.so'
+    pkcs11 = PyKCS11.PyKCS11Lib()
+    pkcs11.load(lib)
+    slot = pkcs11.getSlotList(tokenPresent=True)[0]
     session = pkcs11.openSession(slot)
     cert_obj = session.findObjects([
                   (PyKCS11.CKA_CLASS, PyKCS11.CKO_CERTIFICATE),
@@ -43,12 +24,15 @@ def get_cert_data(slot):
     return cert_der_data
 
 
-def sign_message(text, slot):
+def sign_message(text):
     """ sign message with card 
     :param text: text to sign
-    :param slot: slot number
     :return: signature
     """
+    lib = '/usr/lib/x86_64-linux-gnu/pkcs11/opensc-pkcs11.so'
+    pkcs11 = PyKCS11.PyKCS11Lib()
+    pkcs11.load(lib)
+    slot = pkcs11.getSlotList(tokenPresent=True)[0]
     session = pkcs11.openSession(slot, PyKCS11.CKF_SERIAL_SESSION | PyKCS11.CKF_RW_SESSION)
     session.login('1111')
     criteria = [(PyKCS11.CKA_CLASS, PyKCS11.CKO_PRIVATE_KEY), (PyKCS11.CKA_KEY_TYPE, PyKCS11.CKK_RSA), (PyKCS11.CKA_LABEL, 'CITIZEN AUTHENTICATION KEY' )]
@@ -67,9 +51,7 @@ def validate_signature(signature, text, cert_der_data):
     :param cert_der_data: certificate data
     :return: True if signature is valid, False otherwise
     """
-
     cert = x509.load_der_x509_certificate(cert_der_data, db())
-
     md = Hash(SHA1(), backend=db())
     md.update(text)
     digest = md.finalize()
@@ -82,23 +64,21 @@ def validate_signature(signature, text, cert_der_data):
         return False
 
 def get_name_and_number(cert_data):
+    """ get name and citizen number from certificate
+    :param cert_data: certificate data
+    :return: name and citizen number
+    """
     cert = x509.load_der_x509_certificate(cert_data, db())
     name  = cert.subject.get_attributes_for_oid(x509.NameOID.COMMON_NAME)[0].value
     citizen_number = cert.subject.get_attributes_for_oid(x509.NameOID.SERIAL_NUMBER)[0].value
     return name, citizen_number
 
 def main():
-    auth_slot = pkcs11.getSlotList(tokenPresent=True)[0]
-    # text = b'text to sign'
-    cert_der_data = get_cert_data(auth_slot)
-    # signature = sign_message(text, auth_slot)
-    # print(validate_signature(signature, text, cert_der_data))
-    
-    #print(get_name(auth_slot))
+    text = b'text to sign'
+    cert_der_data = get_cert_data()
+    signature = sign_message(text)
+    print(validate_signature(signature, text, cert_der_data))
     print(get_name_and_number(cert_der_data))    
 
 if __name__ == '__main__':
-    lib = '/usr/lib/x86_64-linux-gnu/pkcs11/opensc-pkcs11.so'
-    pkcs11 = PyKCS11.PyKCS11Lib()
-    pkcs11.load(lib)
     main()
